960f51a9e448e8f3b4cb42fcd199b68f46eb9391
Joshua Wolsborn <wolsborn@augusta.cs.pdx.edu>
2019-04-18 13:09:12 -0700

    added leap

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,21 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+	
+	let mut final_string = String::new();
+	
+	if list.len() == 1{
+
+		final_string.push_str(&format!("And all for the want of a {}.", list[0]));
+		return final_string
+	
+	}else if list.len() >= 2{
+		
+		for i in 0..list.len()-1{
+			final_string.push_str(&format!("For want of a {} the {} was lost.\n", list[i], list[i+1]));
+		}
+
+		final_string.push_str(&format!("And all for the want of a {}.", list[0]));
+		return final_string			
+		
+	}		
+	final_string
 }
