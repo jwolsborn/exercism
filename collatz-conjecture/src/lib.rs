4a242a9bafa42508060ddb95e5717b3715afd3a8
Joshua Wolsborn <wolsborn@augusta.cs.pdx.edu>
2019-04-25 12:21:16 -0700

    Added saddled finished collatz

--- collatz-conjecture/src/lib.rs
+++ collatz-conjecture/src/lib.rs
@@ -1,6 +1,20 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    unimplemented!(
-        "return Some(x) where x is the number of steps required to reach 1 starting with {}",
-        n,
-    )
+	
+	match n {
+	0 => None,
+	_ => {
+		let mut count = 0;
+		let mut m = n;
+		while m != 1 {
+			if m % 2 == 0 {
+				m = m / 2;
+				count = count + 1;
+			}else{
+				m = m * 3 + 1;
+				count = count + 1;
+			}	
+		}
+
+		Some(count)},
+	}
 }
