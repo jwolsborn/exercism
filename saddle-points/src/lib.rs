55c7cb62dc885efa7b6315eafae0045d1ed409a9
Joshua Wolsborn <wolsborn@babbage.cs.pdx.edu>
2019-04-27 19:02:38 -0700

    finished saddle

--- saddle-points/src/lib.rs
+++ saddle-points/src/lib.rs
@@ -1,33 +1,52 @@
+use std::collections::HashSet;
+
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
     
 	let mut solution = Vec::new();
 	
-	for i in 0..input.len()-1{
-		let mut iter_one:usize = 0;
-		let iter_two:usize = i;
-
-		for j in 0..input[i].len()-1{
-			let mut min:u64 = input[i][0];
-			if min >= input[i][j]{
-				min = input[i][j];
-				iter_one = j;
-			}
-		}
-		
-		let mut max:u64 = input[i][iter_one];
-
-		for k in 0..input.len()-1{
-			if max < input[k][iter_one]{
-				max = input[k][iter_one];
-				break;
-			}
-		}
-		
-		if max == input[i][iter_one]{
-			solution.push((iter_two, iter_one));
-		}	
-		
-	 }
 	
+	if input.len() != 0 && input[0].len() != 0{
+
+		let mut mins = HashSet::new();
+		let mut maxs = HashSet::new();
+
+		for i in 0..input.len(){
+			for j in 0..input[i].len(){
+				if is_max(&input[i], &input[i][j]) == true{	
+					maxs.insert((i, j));
+				}
+				
+				if is_min(&input, &input[i][j], j) == true{
+					mins.insert((i, j));
+				}			
+			}
+		}
+		let intersection: HashSet<_> = mins.intersection(&maxs).collect();
+		for x in intersection.iter(){
+			solution.push(**x);
+		}
+	}
 	solution
 }
+
+fn is_max(input: &Vec<u64>,val: &u64) -> bool {
+
+	for i in 0..input.len(){
+			if input[i] > *val {
+			return false;
+		}
+	}
+
+	true	
+}
+
+fn is_min(input: &[Vec<u64>], val: &u64, col_idx: usize) -> bool{
+
+	for i in 0..input.len(){
+			if input[i][col_idx] < *val {
+			return false;
+		}
+	}
+	
+	true
+}	
